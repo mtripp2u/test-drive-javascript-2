6abb7df59f896b23d4291dce4c32a40220229e1a
Michelle <Mtripp2u@Yahoo.com>
2019-10-09 18:24:45 -0400

    plus and minus

--- public/main.js
+++ public/main.js
@@ -1,7 +1,48 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let teamOneScore = 0
+let teamTwoScore = 0
+
+const teamOneScoreText = document.querySelector('.team-1-score')
+teamOneScoreText.textContent = teamOneScore
+
+const teamTwoScoreText = document.querySelector('.team-2-score')
+teamTwoScoreText.textContent = teamTwoScore
+
+const teamOneAddOne = () => {
+  teamOneScore += 1
+  teamOneScoreText.textContent = teamOneScore
+  if (teamOneScore > 7)
+{
+  teamOneScoreText.style.color = 'yellow'
+}
+if (teamOneScore === 10){
+  scoreReachesTen()
+}}
+
+const teamOneSubtractOne = () => {
+  teamOneScore -= 1
+  teamOneScoreText.textContent = teamOneScore
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const teamTwoAddOne = () => {
+  teamTwoScore += 1
+  teamTwoScoreText.textContent = teamTwoScore
+}
+
+const teamTwoSubtractOne = () => {
+  teamTwoScore -= 1
+  teamTwoScoreText.textContent = teamTwoScore
+  }
+  
+  const main = () => {
+  
+  document.querySelector('button.team-1-add-1-button').addEventListener('click', teamOneAddOne)
+
+  document.querySelector('button.team-1-subtract-1-button').addEventListener('click', teamOneSubtractOne)
+  
+  document.querySelector('.team-2-add-1-button').addEventListener('click', teamTwoAddOne)
+
+  document.querySelector('.team-2-subtract-1-button').addEventListener('click', teamTwoSubtractOne)
+
+
+}
+  document.addEventListener('DOMContentLoaded', main)
